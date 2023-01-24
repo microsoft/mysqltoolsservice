0f3a1dfcfa2f562e1be183da44b912ba3c4a197d
nitishsharma25 <nitishsharma@microsoft.com>
2023-01-24 13:10:32 +0530

    added checks before executing autocommit query

--- ossdbtoolsservice/query/query.py
+++ ossdbtoolsservice/query/query.py
@@ -127,7 +127,6 @@ class Query:
 
         # Run each batch sequentially
         try:
-            current_auto_commit_status = connection.execute_query('select @@autocommit;')
             # When Analyze Explain is used we have to disable auto commit
             if self._disable_auto_commit:
                 connection.execute_query('Set autocommit = 0;')
@@ -140,8 +139,8 @@ class Query:
 
                 batch.execute(connection)
         finally:
-            if connection.open:
-                connection.execute_query('Set autocommit = {};'.format(current_auto_commit_status))
+            if connection.open and self._disable_auto_commit:
+                connection.execute_query('Set autocommit = 1;')
             self._execution_state = ExecutionState.EXECUTED
 
     def get_subset(self, batch_index: int, start_index: int, end_index: int):
