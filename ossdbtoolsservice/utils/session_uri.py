e5ba57eeae0427ff0cc024644de4ac34c3675c66
nitishsharma25 <nitishsharma@microsoft.com>
2023-04-10 12:23:31 +0530

    u

--- ossdbtoolsservice/utils/session_uri.py
+++ ossdbtoolsservice/utils/session_uri.py
@@ -3,8 +3,8 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-def is_none_or_empty(value_to_check):
-    if value_to_check is None or len(str(value_to_check).strip()) == 0:
+def is_none_or_empty(value_to_check: str):
+    if value_to_check is None or len(value_to_check.strip()) == 0:
         return True
     return False
 
@@ -13,7 +13,7 @@ def generate_session_uri(params):
         params.options['host'] if params.options.get('host') and not is_none_or_empty(params.options.get('host')) else 'NULL',
         params.options['dbname'] if params.options.get('dbname') and not is_none_or_empty(params.options.get('dbname')) else 'NULL',
         params.options['user'] if params.options.get('user') and not is_none_or_empty(params.options.get('user')) else 'NULL',
-        params.options['port'] if params.options.get('port') and not is_none_or_empty(params.options.get('port')) else 'NULL',
+        params.options['port'] if params.options.get('port') and not is_none_or_empty(str(params.options.get('port'))) else 'NULL',
         params.options['authenticationType'] if params.options.get('authenticationType') and not is_none_or_empty(params.options.get('authenticationType')) else 'NULL',
         params.options['groupId'] if params.options.get('groupId') and not is_none_or_empty(params.options.get('groupId')) else 'NULL'
     )
