1f6bf751a5541a1933006fbe212cf11021198700
nitishsharma25 <nitishsharma@microsoft.com>
2023-04-10 11:00:55 +0530

    added validate params for session uri

--- ossdbtoolsservice/utils/session_uri.py
+++ ossdbtoolsservice/utils/session_uri.py
@@ -3,18 +3,35 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-def is_not_none_or_empty(value_to_check: str):
-    if value_to_check is None or len(value_to_check.strip()) == 0:
-        return False
-    return True
+from ossdbtoolsservice.utils.constants import DEFAULT_PORT
 
-def generate_session_uri(params):
+def is_none_or_empty(value_to_check: str):
+    if value_to_check is None or len(value_to_check.strip()) == 0:
+        return True
+    return False
+
+def validate_params(provider, params):
+    if not params.options.get('host') or is_none_or_empty(params.options.get('host')):
+        params.options['host'] = 'NULL'
+    if not params.options.get('dbname') or is_none_or_empty(params.options.get('dbname')):
+        params.options['dbname'] = 'NULL'
+    if not params.options.get('user') or is_none_or_empty(params.options.get('user')):
+        params.options['user'] = 'NULL'
+    if not params.options.get('port') or is_none_or_empty(params.options.get('port')):
+        params.options['port'] = DEFAULT_PORT[provider]
+    if not params.options.get('authenticationType') or is_none_or_empty(params.options.get('authenticationType')):
+        params.options['authenticationType'] = 'NULL'
+    if not params.options.get('groupId') or is_none_or_empty(params.options.get('groupId')):
+        params.options['groupId'] = 'NULL'
+
+def generate_session_uri(provider, params):
+    validate_params(provider, params)
     session_uri = "{0}:{1}:{2}:{3}:{4}:{5}".format(
-        params.options['host'] if is_not_none_or_empty(params.options['host']) else "NULL",
-        params.options['dbname'] if is_not_none_or_empty(params.options['dbname']) else "NULL",
-        params.options['user'] if is_not_none_or_empty(params.options['user']) else "NULL",
-        params.options['port'] if is_not_none_or_empty(str(params.options['port'])) else "NULL",
-        params.options['authenticationType'] if is_not_none_or_empty(params.options['authenticationType']) else "NULL",
-        params.options['groupId'] if is_not_none_or_empty(params.options['groupId']) else "NULL"
+        params.options['host'],
+        params.options['dbname'],
+        params.options['user'],
+        params.options['port'],
+        params.options['authenticationType'],
+        params.options['groupId']
     )
     return session_uri
