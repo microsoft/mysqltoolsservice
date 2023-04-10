7498b85c6eb8be3a5d4edcbb48d0003b2a1977a2
nitishsharma25 <nitishsharma@microsoft.com>
2023-04-10 11:52:47 +0530

    u

--- ossdbtoolsservice/utils/session_uri.py
+++ ossdbtoolsservice/utils/session_uri.py
@@ -10,11 +10,11 @@ def is_none_or_empty(value_to_check: str):
 
 def generate_session_uri(params):
     session_uri = "{0}:{1}:{2}:{3}:{4}:{5}".format(
-        params.options['host'] if not params.options.get('host') or is_none_or_empty(params.options.get('host')) else 'NULL',
-        params.options['dbname'] if not params.options.get('dbname') or is_none_or_empty(params.options.get('dbname')) else 'NULL',
-        params.options['user'] if not params.options.get('user') or is_none_or_empty(params.options.get('user')) else 'NULL',
-        params.options['port'] if not params.options.get('port') or is_none_or_empty(params.options.get('port')) else 'NULL',
-        params.options['authenticationType'] if not params.options.get('authenticationType') or is_none_or_empty(params.options.get('authenticationType')) else 'NULL',
-        params.options['groupId'] if not params.options.get('groupId') or is_none_or_empty(params.options.get('groupId')) else 'NULL'
+        params.options['host'] if params.options.get('host') and not is_none_or_empty(params.options.get('host')) else 'NULL',
+        params.options['dbname'] if params.options.get('dbname') and not is_none_or_empty(params.options.get('dbname')) else 'NULL',
+        params.options['user'] if params.options.get('user') and not is_none_or_empty(params.options.get('user')) else 'NULL',
+        params.options['port'] if params.options.get('port') and not is_none_or_empty(params.options.get('port')) else 'NULL',
+        params.options['authenticationType'] if params.options.get('authenticationType') and not is_none_or_empty(params.options.get('authenticationType')) else 'NULL',
+        params.options['groupId'] if params.options.get('groupId') and not is_none_or_empty(params.options.get('groupId')) else 'NULL'
     )
     return session_uri
