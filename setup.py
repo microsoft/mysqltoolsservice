5cb07aa926b752209517a79a79489aa6763d3175
rishky-msft <102506628+rishky-msft@users.noreply.github.com>
2024-05-22 11:56:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(
-    build_exe = "build/mysqltoolsservice",
-    packages = ['asyncio', 'jinja2', 'inflection', 'sqlparse',
+buildOptions = {
+    "build_exe": "build/mysqltoolsservice",
+    "packages": ['asyncio', 'jinja2', 'inflection', 'sqlparse',
                 'prompt_toolkit', 'xlsxwriter', 'nose2', 'parameterized', 'coverage', 'autopep8', 'flake8', 'debugpy', '_pydev_runfiles', 'mysql', 'azure'],
-    excludes = [],
-    include_files = [],
-    replace_paths = [("*", "")]
-)
+    "excludes": [],
+    "include_files": [],
+    "replace_paths": [("*", "")]
+}
 
 base = 'Console'
 
@@ -21,6 +21,6 @@ setup(
     name = 'OSS MySQL Tools Service',
     version = '0.1.2',
     description = 'Carbon data protocol server implementation for MySQL',
-    options = dict(build_exe=buildOptions),
+    options = {"build_exe": buildOptions},
     executables = executables
 )
